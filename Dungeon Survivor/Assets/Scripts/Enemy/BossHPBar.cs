9a424d984c8cd3fc31d572252f968b1caaa6249d
namlxhe153241 <namlxhe153241@fpt.edu.vn>
2024-07-18 05:46:22 +0700

    HPBoss

--- Dungeon Survivor/Assets/Scripts/Enemy/BossHPBar.cs
+++ Dungeon Survivor/Assets/Scripts/Enemy/BossHPBar.cs
@@ -1,8 +1,42 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossHPBar : MonoBehaviour
 {
+    //// Tham chiếu đến thanh HP
+    //public Image hpBar;
 
+    //// Giá trị HP hiện tại và HP tối đa của boss
+    //private float currentHP;
+    //private float maxHP = 100f;
+
+    //// Khởi tạo giá trị HP hiện tại
+    //void Start()
+    //{
+    //    currentHP = maxHP;
+    //    UpdateHPBar();
+    //}
+
+    //// Hàm để cập nhật thanh HP
+    //void UpdateHPBar()
+    //{
+    //    hpBar.fillAmount = currentHP / maxHP;
+    //}
+
+    //// Hàm để gây sát thương cho boss
+    //public void TakeDamage(float amount)
+    //{
+    //    currentHP -= amount;
+    //    if (currentHP < 0) currentHP = 0;
+    //    UpdateHPBar();
+    //}
+
+    //// Hàm để hồi phục HP cho boss (nếu cần)
+    //public void Heal(float amount)
+    //{
+    //    currentHP += amount;
+    //    if (currentHP > maxHP) currentHP = maxHP;
+    //    UpdateHPBar();
+    //}
 }
