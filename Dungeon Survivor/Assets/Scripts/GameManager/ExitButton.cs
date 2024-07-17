7a9d6f4e9ae1d0caa8608a834e133fdda2d6c4e8
Toan Tran <tranthetoan2003@gmail.com>
2024-07-18 00:58:32 +0700

    rebuild

--- Dungeon Survivor/Assets/Scripts/GameManager/ExitButton.cs
+++ Dungeon Survivor/Assets/Scripts/GameManager/ExitButton.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitButton : MonoBehaviour
 {
+    private void Awake()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(QuitApplication);
+    }
+
     public void QuitApplication()
     {
         Application.Quit();
